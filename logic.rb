2cf3c39ac8327fb2fe772a8e65c5b2d85afcea1b
Simon Chapman <sdchapman@users.noreply.github.com>
2018-09-04 11:17:52 +0100

    Improve wording of initial question

--- logic.rb
+++ logic.rb
@@ -105,7 +105,7 @@ if ['prep'].include?(ARGV[0])
     puts ''
     puts colorize_green("This dev env has been provisioned to run for the repo: #{File.read(DEV_ENV_CONTEXT_FILE)}")
   else
-    print colorize_yellow('Please enter the url of your dev env repo (SSH): ')
+    print colorize_yellow('Please enter the (Git) url of your dev env configuration repository: ')
     app_grouping = STDIN.gets.chomp
     File.open(DEV_ENV_CONTEXT_FILE, 'w+') { |file| file.write(app_grouping) }
   end
