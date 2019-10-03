de01d92dddbdbdae758448e75f4485ed506b2b50
jacob gathercole <jacobgathercole@gmail.com>
2019-10-03 12:03:19 +0100

    added gitlist script to show the current branch of downloaded apps

---
+++ scripts/git_list.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+dir="$1"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+NORMAL="\033[0;39m"
+# No directory has been provided, use current
+if [ -z "$dir" ]
+then
+    dir="`dirname $0`/../apps/"
+fi
+# Make sure directory ends with "/"
+if [[ $dir != */ ]]
+then
+	dir="$dir/*"
+else
+	dir="$dir*"
+fi
+for item in $dir
+do
+	if [ -d $item ] && [ -d "${item}/.git" ]; then
+		(
+			cd $item;
+			repo=$(basename `git rev-parse --show-toplevel`)
+			branch=$(git branch | grep \* | cut -d ' ' -f2)
+			if [ $branch == "master" ];
+			then
+				branch_colour=$NORMAL
+			else
+				branch_colour=$YELLOW
+			fi
+			echo -e "${GREEN} ${repo} ${NORMAL} -> ${branch_colour} ${branch}"
+		);
+	fi
+done
