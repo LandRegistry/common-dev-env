fc9adcdde1ff52254a99f95410b33e23d2540d91
Simon Chapman <7984422+sichapman@users.noreply.github.com>
2020-06-22 16:56:47 +0100

    Add quickup and quickreload commands
    This involves full parameterisation of logic.rb

--- scripts/docker_prepare.sh
+++ scripts/docker_prepare.sh
@@ -20,18 +20,6 @@ export OUTSIDE_GID=$(id -g)
 dockerfilelist=$(<./.docker-compose-file-list)
 export COMPOSE_FILE=$dockerfilelist
 
-echo "- - - Removing any orphaned docker volumes - - -"
-volumes=$(docker volume ls -qf dangling=true)
-if [ -n "$volumes" ]; then
-  docker volume rm `docker volume ls -q -f dangling=true`
-fi
-
-echo "- - - Removing any orphaned docker images - - -"
-images=$(docker images -f dangling=true -q)
-if [ -n "$images" ]; then
-  docker rmi -f `docker images -f dangling=true -q`
-fi
-
 # If there's docker apps (the var is not empty), then do docker stuff
 if ! [ -z "$COMPOSE_FILE" ]; then
   # Only "up" creates network so lets create it here first in case someone does a "start"
