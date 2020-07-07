c0d13a078a18720b3f6eae66e44aff510e4116d6
Simon Chapman <7984422+sichapman@users.noreply.github.com>
2020-07-07 14:02:25 +0100

    Add check that run.sh has been sourced

--- run.sh
+++ run.sh
@@ -1,3 +1,22 @@
+# Best effort check that the script has been sourced.
+# From https://stackoverflow.com/a/28776166
+sourced=0
+if [ -n "$ZSH_EVAL_CONTEXT" ]; then
+  case $ZSH_EVAL_CONTEXT in *:file) sourced=1;; esac
+elif [ -n "$KSH_VERSION" ]; then
+  [ "$(cd $(dirname -- $0) && pwd -P)/$(basename -- $0)" != "$(cd $(dirname -- ${.sh.file}) && pwd -P)/$(basename -- ${.sh.file})" ] && sourced=1
+elif [ -n "$BASH_VERSION" ]; then
+  (return 0 2>/dev/null) && sourced=1
+else # All other shells: examine $0 for known shell binary filenames
+  # Detects `sh` and `dash`; add additional shell filenames as needed.
+  case ${0##*/} in sh|dash) sourced=1;; esac
+fi
+
+if test $sourced -eq 0; then
+    echo -e "\e[36mIt looks like you have executed the script directly instead of sourcing it. This will cause problems due to unset environment variables afterwards. I'll give you 10 seconds to CTRL-C out before continuing...\e[0m"
+    sleep 10
+fi
+
 command="$1"
 if [ "$command" = "up" ]
 then
