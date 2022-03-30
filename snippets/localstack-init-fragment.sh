6af739c9cda3990a05243d3b80bbc282ba932af5
Leigh Brooks <leigh.brooks@landregistry.gov.uk>
2022-03-30 20:10:20 +0100

    SPG fix

--- snippets/localstack-init-fragment.sh
+++ snippets/localstack-init-fragment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This is an example custom-provision script that creates a new S3 bucket in localstack (if it doesn't already exist),
+# This is an example fragment that creates a new S3 bucket in localstack (if it doesn't already exist),
 # wipes the contents of said bucket, and enables versioning on said bucket.
 # Each command will be run sequentially in the container.
 awslocal s3 mb s3://example-bucket
