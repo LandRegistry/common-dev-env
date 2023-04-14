479777e0075904185097c9269e6af57d08f11ad6
Simon Chapman <sdchapman@gmail.com>
2023-04-14 11:08:07 +0100

    Cope with standalone Docker
    Where you access Compose via `docker compose` only, and `docker-compose` does not exist

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 export DC_VERSION=1
-docker-compose version 2>&1 | grep -q 'version 2\|version v2' && DC_VERSION=2 && echo -e "\e[35mUsing Docker-Compose version 2 commands\e[0m"
+
+# No docker-compose command means v2 is implied
+docker-compose > /dev/null 2>&1
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  DC_VERSION=2
+else
+  docker-compose version 2>&1 | grep -q 'version 2\|version v2' && DC_VERSION=2
+fi
 
 if [ "$DC_VERSION" = "2" ] ; then
+  echo -e "\e[35mUsing Docker-Compose version 2 commands\e[0m"
   export DC_CMD='docker compose'
 else
   export DC_CMD='docker-compose --compatibility'
